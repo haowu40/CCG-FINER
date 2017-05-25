97c68c893b6723807c06fede023a0e5e7350ca54
Hao Wu <haowu@haowu.me>
2017-05-25 08:25:26 -0500

    update

--- src/main/java/edu/illinois/cs/cogcomp/lbjava/learn/PipelineTest.java
+++ src/main/java/edu/illinois/cs/cogcomp/lbjava/learn/PipelineTest.java
@@ -3,6 +3,7 @@ package edu.illinois.cs.cogcomp.lbjava.learn;
 import edu.illinois.cs.cogcomp.annotation.AnnotatorException;
 import edu.illinois.cs.cogcomp.annotation.AnnotatorServiceConfigurator;
 import edu.illinois.cs.cogcomp.annotation.BasicAnnotatorService;
+import edu.illinois.cs.cogcomp.annotation.TextAnnotationBuilder;
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation
         .TextAnnotation;
@@ -12,14 +13,13 @@ import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
 import edu.illinois.cs.cogcomp.finer.ner_test.NERAnnotatorPub;
 import edu.illinois.cs.cogcomp.lbjava.learn.SparseAveragedPerceptron;
 import edu.illinois.cs.cogcomp.ner.NERAnnotator;
+import edu.illinois.cs.cogcomp.nlp.tokenizer.StatefulTokenizer;
+import edu.illinois.cs.cogcomp.nlp.utility.TokenizerTextAnnotationBuilder;
 import edu.illinois.cs.cogcomp.pipeline.main.PipelineFactory;
 import edu.illinois.cs.cogcomp.wsd.annotators.WordSenseAnnotator;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -84,25 +84,64 @@ public class PipelineTest {
 
     public static void main(String[] args) throws IOException,
             AnnotatorException {
+        TextAnnotationBuilder tab;
+        // don't split on hyphens, as NER models are trained this way
+        tab = new TokenizerTextAnnotationBuilder(new StatefulTokenizer());
+        String file = "/home/haowu4/data/1blm/text/train/news.en-00001-of-00100";
+
+
+        Scanner s=new Scanner(System.in);
+
+        System.out.println(" Press enter to load model.....");
+        s.nextLine();
+        System.out.println(" Start load model.....");
+
 
 //        BasicAnnotatorService bas = getPipeline();
-        NERAnnotatorPub co = new NERAnnotatorPub(ViewNames.NER_CONLL);
+        NERAnnotatorPub co = new NERAnnotatorPub(ViewNames.NER_ONTONOTES);
         co.initialize(new ResourceManager(new Properties()));
-        OVector ov = co.t1.getNetwork();
-        for (int i = 0; i < ov.size(); i++) {
-            SparseAveragedPerceptron svp = (SparseAveragedPerceptron) ov.get(i);
-            double[] d = svp.awv.averagedWeights.toArray();
-            ;
-            try (PrintWriter out = new PrintWriter("/tmp/" + i + ".txt")) {
-                for (double v : d) {
-                    out.write(String.valueOf(v));
-                    out.write("\n");
+        TextAnnotation ta = tab.createTextAnnotation("", "", "this is a example.");
+        co.getView(ta);
+        System.out.println(" Load model Done.....");
+
+//        OVector ov = co.t1.getNetwork();
+//        for (int i = 0; i < ov.size(); i++) {
+//            SparseAveragedPerceptron svp = (SparseAveragedPerceptron) ov.get(i);
+//            double[] d = svp.awv.averagedWeights.toArray();
+//            ;
+//            try (PrintWriter out = new PrintWriter("/tmp/vecs/" + i + ".txt")) {
+//                for (double v : d) {
+//                    out.write(String.valueOf(v));
+//                    out.write("\n");
+//                }
+//            }
+//
+//
+//        }
+//        System.out.println(ov.size());
+
+        System.out.println("[L109] Press enter to Annotate Docs .....");
+        s.nextLine();
+        System.out.println(" Start annotate data.....");
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            int c = 0;
+            while ((line = br.readLine()) != null) {
+                ta = tab.createTextAnnotation("", "", line);
+                co.getView(ta);
+                c++;
+                if (c == 10000){
+                    break;
                 }
             }
-
-
         }
-        System.out.println(ov.size());
+        System.out.println(" Annotation Done.....");
+        System.out.println("[L130] Press enter to Exit .....");
+        s.nextLine();
+
+
 
     }
 }
